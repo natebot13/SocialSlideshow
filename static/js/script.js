e954b095a1046d4171efe2d376f2164faf4fb3a5
Nathan Pannell <nathan.pannell@berkeley.edu>
2015-12-24 14:01:17 -0800

    first test

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,7 @@ var images = [];
 var xmlhttp = new XMLHttpRequest();
 
 xmlhttp.onreadystatechange = function() {
+    console.log(xmlhttp.responseText);
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         var images = JSON.parse(xmlhttp.responseText);
         chooseNextImage();
